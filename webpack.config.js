ea654fc5b51fcb31354a7e40693ed53bb7148a13
Piotr G <noschematic@gmail.com>
2018-03-16 15:47:35 +0100

    Fixed path to sass in dev tools and autoprefixer issue.

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,21 @@ const path = require('path')
 const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-const appThemeSrc =  '../WebpackWorkflowTest/WebpackWorkflowTest/App_Themes/test1';
+const appThemeName = 'WebpackSassTest';
+
+const appThemeSrc =  '../path/to/App_Themes/' + appThemeName;
 
 module.exports = {
   context: path.resolve(__dirname, './'),
   entry: {
-    /* app: './js/index.js',*/
-    css: appThemeSrc + '/_style/Main.scss',
+    /* app: './src/scripts/script.js',*/
+    css: appThemeSrc + '/src/sass/Main.scss',
   },
   output: {
-    path: path.resolve(__dirname, appThemeSrc + '/_style'),
-    publicPath: '/_style/',
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, appThemeSrc + '/dist'),
+    filename: 'bundle.js',   
   },
-  devtool: "source-map",
+  devtool: "nosources-source-map",  
   module: {
     rules: [
     {
@@ -26,13 +27,19 @@ module.exports = {
           {
             loader: 'css-loader',
             options: {
-              minimize: true || {/* CSSNano Options */},
+              // minimize: true || {/* CSSNano Options */},
               sourceMap: true
             }
           },
-          // {
-          //   loader: 'postcss-loader'
-          // },
+          {
+            loader: 'postcss-loader',
+            options: {
+              config: {
+                path: path.resolve(__dirname, './')                
+              },
+              sourceMap: true
+            }
+          },
           {
             loader: 'sass-loader', 
             options: {
@@ -40,7 +47,7 @@ module.exports = {
             }
           }
         ],
-        publicPath: '/dist',
+        publicPath: '../../',
       })
     }]
   },
