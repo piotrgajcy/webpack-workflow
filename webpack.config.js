be105d57f0dcc476bc9df5351247f3fb4e33d4de
Piotr <noschematic@gmail.com>
2018-03-18 22:21:11 +0100

    Fixed problem with creating unnecessary css.bundle.js file

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,24 @@
-const path = require('path')
-const webpack = require('webpack')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const autoprefixer = require('autoprefixer')
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer');
 const babelenv = require('babel-preset-env');
-
-
 
 // const appThemeName = 'WebpackSassTest';
 const appThemeName = 'test1';
 
 // const appThemeSrc =  '../path/to/App_Themes/' + appThemeName;
-const appThemeSrc =  '../WebpackWorkflowTest/WebpackWorkflowTest/App_Themes/' + appThemeName;
+const appThemeSrc = `../WebpackWorkflowTest/WebpackWorkflowTest/App_Themes/${appThemeName}`;
 
 module.exports = {
   context: path.resolve(__dirname, './'),
   entry: {
-    script: appThemeSrc + '/src/scripts/script.js',
-    css: appThemeSrc + '/src/sass/Main.scss',
+    script: [`${appThemeSrc}/src/scripts/script.js`,
+      `${appThemeSrc}/src/sass/Main.scss`],
   },
   output: {
-    path: path.resolve(__dirname, appThemeSrc + '/dist'),
-    filename: '[name].bundle.js',   
+    path: path.resolve(__dirname, `${appThemeSrc}/dist`),
+    filename: '[name].bundle.js',
   },
   module: {
     rules: [
@@ -28,52 +26,53 @@ module.exports = {
         test: /\.js$/,
         use: [{
           loader: 'babel-loader',
-            options: {
-              // babelrc: true,
-              presets: [babelenv]
-            }        
+          options: {
+            // babelrc: true,
+            presets: [babelenv],
+          },
         }],
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.(css|scss)$/,
         use: ExtractTextPlugin.extract({
+          filename: 'css/[name].css',
           fallback: 'style-loader',
           use: [
             {
               loader: 'css-loader',
               options: {
                 // minimize: true || {/* CSSNano Options */},
-                sourceMap: true
-              }
+                sourceMap: true,
+              },
             },
             {
               loader: 'postcss-loader',
               options: {
                 config: {
-                  path: path.resolve(__dirname, './')              
+                  path: path.resolve(__dirname, './css'),
                 },
-                sourceMap: true
-              }
+                sourceMap: true,
+              },
             },
             {
-              loader: 'sass-loader', 
+              loader: 'sass-loader',
               options: {
-                sourceMap: true
-              }
-            }
+                sourceMap: true,
+              },
+            },
           ],
-        })
-      }
-    ]
+        }),
+      },
+    ],
   },
 
   plugins: [
     new ExtractTextPlugin('Main.css'),
     new webpack.SourceMapDevToolPlugin({
-      filename: "[file].map",
-      exclude: ["script.bundle.js", "css.bundle.js"],
-      noSources: true
-    })
-  ]
-}
+      filename: '[file].map',
+      exclude: ['script.bundle.js', 'css.bundle.js'],
+      noSources: true,
+    }),
+  ],
+};
